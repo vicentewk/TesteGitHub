c2f02d2eba23a6f8a9e2a2447759b558a99cef32
vicen <vicen@192.168.1.4>
2019-04-04 23:33:29 -0300

    criação da variável teste

--- TesteGitHub/src/testegithub/TesteGitHub.java
+++ TesteGitHub/src/testegithub/TesteGitHub.java
@@ -11,11 +11,13 @@ package testegithub;
  */
 public class TesteGitHub {
 
+    private int teste;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
